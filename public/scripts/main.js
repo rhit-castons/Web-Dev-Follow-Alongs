ecfb2bac69b7e4ba6dc3db8423a7b31650b13de6
Sofia <castons@rose-hulman.edu>
2023-09-11 23:55:34 -0400

    first commit

---
+++ public/scripts/main.js
@@ -0,0 +1,13 @@
+function paint(color) {
+    const circle = document.getElementById('circleID');
+    circle.style = `background-color:${color}`;
+}
+
+function paint(color) {
+    console.log(color);
+    const circle = document.getElementById('circleID');
+    circle.style = `background-color:${color}`;
+    console.log(circle);
+}
+
+console.log('Script Loaded...');
